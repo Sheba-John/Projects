5fd3ebcc3358ed7d2a090f36c4fe3d06ea0e00a7
Sheba-John <92512577+Sheba-John@users.noreply.github.com>
2021-11-03 16:25:29 +0530

    Add files via upload

---
+++ SpringOrm2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOrm2Application.class, args);
+	}
+
+}
